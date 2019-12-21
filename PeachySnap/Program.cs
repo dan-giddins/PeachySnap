70c1604cc7ebd0c0f9a0d8b4a367cf2e53ad1f31
Dan Giddins <daniel.giddins@nottingham.ac.uk>
2019-12-21 10:54:33 +0000

    Add project files.

---
+++ PeachySnap/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PeachySnap
+{
+	class Program
+	{
+		static void Main()
+		{
+			// generate deck
+			var deck = Enumerable.Range(1, 52);
+			
+		}
+	}
+}
