d5f3eed281140d113501f5df650fb504bbd02f8a
Dan Giddins <daniel.giddins@nottingham.ac.uk>
2019-12-21 16:50:49 +0000

    first try

--- PeachySnap/Program.cs
+++ PeachySnap/Program.cs
@@ -12,6 +12,7 @@ namespace PeachySnap
 		private const int FirstHalf = (NumberOfCards + 1)/2;
 		// half deck (rounded down)
 		private const int SecondHalf = NumberOfCards - FirstHalf;
+		private const int SuitSize = 13;
 
 		static void Main()
 		{
@@ -38,27 +39,44 @@ namespace PeachySnap
 			var deckB = new Queue<int>(shuffledDeck.GetRange(FirstHalf, SecondHalf));
 			// create table
 			var table = new List<int>();
+			var roundCounter = 0;
+			Console.WriteLine("Player A's cards:");
+			Console.WriteLine(deckA);
+			Console.WriteLine("Player B's cards:");
+			Console.WriteLine(deckB);
 			// play game
 			while (deckA.Any() && deckB.Any())
 			{
-				// play a card, modded down to 1 - 13
-				var card = (deckA.Dequeue() - 1) % 13 + 1;
-				// add card to the table
-				table.Add(card);
-				// get the index of the card with the same number
-				var rangeIndex = table.IndexOf(card);
-				// if there is a card on the table that matches
-				if (rangeIndex > -1)
-				{
-					// get the number of cards to take
-					var rangeCount = table.Count - rangeIndex;
-					// get the range of cards from the table
-					var range = table.GetRange(rangeIndex, rangeCount);
-					// remove these cards from the table
-					table.GetRange(rangeIndex, rangeCount);
-					// add cards to hand
-					range.ForEach(x => deckA.Enqueue(x));
-				}
+				roundCounter++;
+				Console.WriteLine($"Round {roundCounter}");
+				Console.WriteLine("Player A's cards:");
+				Console.WriteLine(deckA);
+				Console.WriteLine("Player B's cards:");
+				Console.WriteLine(deckB);
+				Move(deckA, table);
+				Move(deckB, table);
+			}
+		}
+
+		private static void Move(Queue<int> deck, List<int> table)
+		{
+			// play a card, modded down to 1 - 13
+			var card = (deck.Dequeue() - 1) % SuitSize + 1;
+			// add card to the table
+			table.Add(card);
+			// get the index of the card with the same number
+			var rangeIndex = table.IndexOf(card);
+			// if there is a card on the table that matches
+			if (rangeIndex > -1)
+			{
+				// get the number of cards to take
+				var rangeCount = table.Count - rangeIndex;
+				// get the range of cards from the table
+				var range = table.GetRange(rangeIndex, rangeCount);
+				// remove these cards from the table
+				table.GetRange(rangeIndex, rangeCount);
+				// add cards to hand
+				range.ForEach(x => deck.Enqueue(x));
 			}
 		}
 	}
