ea5aeae09eb77e26586b389d945dfb297429eb1d
Dan Giddins <daniel.giddins@nottingham.ac.uk>
2019-12-21 18:20:12 +0000

    write to csv

--- PeachySnap/Program.cs
+++ PeachySnap/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 
 namespace PeachySnap
@@ -9,17 +10,41 @@ namespace PeachySnap
 		// number of cards
 		private const int NumberOfCards = 52;
 		// half deck (rounded up)
-		private const int FirstHalf = (NumberOfCards + 1)/2;
+		private const int FirstHalf = (NumberOfCards + 1) / 2;
 		// half deck (rounded down)
 		private const int SecondHalf = NumberOfCards - FirstHalf;
 		private const int SuitSize = 13;
+		private const int NumberOfGames = 100000;
 
 		static void Main()
 		{
 			// generate pack of 'cards'
 			var cards = Enumerable.Range(1, NumberOfCards);
-			// copy to temporary deck
-			var holdingDeck = cards.ToList();
+			var gameCounter = 0;
+			var aWins = 0;
+			var roundLengths = new List<int>();
+			while (gameCounter < NumberOfGames)
+			{
+				gameCounter++;
+				// play game
+				(var winner, var roundCounter) = PlayAGame(cards.ToList());
+				if (winner == "A")
+				{
+					aWins++;
+				}
+				roundLengths.Add(roundCounter);
+			}
+			Console.WriteLine($"After {NumberOfGames} games...");
+			Console.WriteLine($"A won {aWins} times ({aWins / (float) NumberOfGames * 100}%)");
+			Console.WriteLine($"Average round length: {roundLengths.Average()}");
+			var csv = string.Empty;
+			roundLengths.ForEach(x => csv += $"{x},");
+			csv = csv[..^1];
+			File.WriteAllText(@"C:\temp\rounds.csv", csv);
+		}
+
+		private static (string winner, int roundCounter) PlayAGame(IList<int> holdingDeck)
+		{
 			// shuffle deck
 			var shuffledDeck = new List<int>();
 			var random = new Random();
@@ -40,32 +65,40 @@ namespace PeachySnap
 			// create table
 			var table = new List<int>();
 			var roundCounter = 0;
-			Console.WriteLine("Player A's cards:");
-			Console.WriteLine(deckA);
-			Console.WriteLine("Player B's cards:");
-			Console.WriteLine(deckB);
 			// play game
 			while (deckA.Any() && deckB.Any())
 			{
 				roundCounter++;
-				Console.WriteLine($"Round {roundCounter}");
-				Console.WriteLine("Player A's cards:");
-				Console.WriteLine(deckA);
-				Console.WriteLine("Player B's cards:");
-				Console.WriteLine(deckB);
 				Move(deckA, table);
 				Move(deckB, table);
 			}
+			return (winner: deckA.Any() ? "A" : "B", roundCounter);
+		}
+
+		private static void PrintDecks(Queue<int> deckA, Queue<int> deckB)
+		{
+			Console.WriteLine("Player A's cards:");
+			foreach (var card in deckA)
+			{
+				Console.Write($"{card} ");
+			}
+			Console.WriteLine();
+			Console.WriteLine("Player B's cards:");
+			foreach (var card in deckB)
+			{
+				Console.Write($"{card} ");
+			}
+			Console.WriteLine();
 		}
 
 		private static void Move(Queue<int> deck, List<int> table)
 		{
 			// play a card, modded down to 1 - 13
 			var card = (deck.Dequeue() - 1) % SuitSize + 1;
-			// add card to the table
-			table.Add(card);
 			// get the index of the card with the same number
 			var rangeIndex = table.IndexOf(card);
+			// add card to the table
+			table.Add(card);
 			// if there is a card on the table that matches
 			if (rangeIndex > -1)
 			{
@@ -74,7 +107,7 @@ namespace PeachySnap
 				// get the range of cards from the table
 				var range = table.GetRange(rangeIndex, rangeCount);
 				// remove these cards from the table
-				table.GetRange(rangeIndex, rangeCount);
+				table.RemoveRange(rangeIndex, rangeCount);
 				// add cards to hand
 				range.ForEach(x => deck.Enqueue(x));
 			}
